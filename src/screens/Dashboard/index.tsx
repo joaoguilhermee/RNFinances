93519b5343cd0c03eab36d1b726f2a63d593364d
Joao Guilherme Barbosa <hello@joaoguilher.me>
2022-07-28 10:31:29 -0300

    WIP: header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserInfo,
+  User,
+  UserGreeting,
+  Photo,
+  UserName,
+  UserWrapper,
+} from './styles';
 
 export const Dashboard = () => {
   return (
     <Container>
-      <Title>Dasboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/4670403?v=4',
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>João Guilherme</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 };
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,14 +1,40 @@
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
   width: 100%;
-  justify-content: center;
-  align-items: center;
   background: ${({ theme }) => theme.colors.secondary};
 `;
 
-export const Title = styled.Text`
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+  background: ${({ theme }) => theme.colors.primary};
+`;
+
+export const UserWrapper = styled.View`
+  width: 100%;
+  padding: 0 24px;
+`;
+export const UserInfo = styled.View`
+  flex-direction: row;
+`;
+export const User = styled.View`
+  margin-left: 17px;
+`;
+export const Photo = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  border-radius: 10px;
+`;
+export const UserGreeting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
   font-family: ${({ theme }) => theme.fonts.bold};
-  color: ${({ theme }) => theme.colors.primary};
 `;
